1bc63823169d7a4450177b1a803b2caf1990e2d2
Andrea Rosero <andrea.rosero.p@gmail.com>
2024-11-25 00:59:24 -0300

    avance estructura carrito, card de productos y car de resumen de compra

--- src/pages/Carrito.jsx
+++ src/pages/Carrito.jsx
@@ -1,4 +1,4 @@
-import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import { Container, Row, Col, Card, Button, ListGroup} from "react-bootstrap";
 
 const Carrito = () => {
   return (
@@ -8,41 +8,80 @@ const Carrito = () => {
         container
         <Row style={{ backgroundColor: "pink" }}>
           <Col
-            className="ps-5 pe-5 p-3"
+            className="ps-3 pe-3 p-3"
             md={8}
             style={{ backgroundColor: "yellow" }}
           >
             COLUMNA INSIDE DE ROW, YELLOW
-            <Card className="text-center">
-          
-              <Card.Img variant="top" src="holder.js/100px180" />
-              <Card.Body>
-                <Card.Title>Special title treatment</Card.Title>
-                <Card.Text>
-                  With supporting text below as a natural lead-in to additional
-                  content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-              <Card.Footer className="text-muted">2 days ago</Card.Footer>
+            <Card className="">
+              <Row className="g-0">
+                <Col md={3} style={{ backgroundColor: "blue" }}>
+                  <Card.Img variant="top" src="holder.js/100px180" />
+                </Col>
+
+                <Col md={9} style={{ backgroundColor: "white" }}>
+                  <Card.Body>
+                    <Card.Title>Nombre</Card.Title>
+                    <Card.Text> Precio $ </Card.Text>
+                    <Row className="g-0 d-flex justify-content-between">
+                      <Col className=" justify-content-start">
+                        <Button variant="outline-danger" size="sm">
+                          Eliminar Producto
+                        </Button>
+                      </Col>
+
+                      <Col className="d-flex justify-content-end">
+                        <Button className="me-1" variant="secondary">
+                          - 1
+                        </Button>
+                        <Button
+                          className="me-1"
+                          variant="outline-success"
+                          disabled
+                        >
+                          1Contador
+                        </Button>
+                        <Button className="me-1" variant="info">
+                          + 1
+                        </Button>
+                      </Col>
+                    </Row>
+                  </Card.Body>
+                </Col>
+              </Row>
             </Card>
-            <Row>
-              <Col md={3} style={{ backgroundColor: "blue" }}>
-                {" "}
-                {/* Contenido de la columna de 3 */} COLUMNA DE TRES DONDE
-                DEBERIA ESTAR LA FOTO DEL PRIMER PRODUCTO{" "}
-              </Col>{" "}
-              <Col md={9} style={{ backgroundColor: "white" }}>
-                {" "}
-                {/* Contenido de la columna de 9 */} DETALLE DEL PRODUCTO QUE
-                ESTÁ COMPRANDO
-              </Col>
-            </Row>
-          </Col>{" "}
+          </Col>
           <Col md={4} style={{ backgroundColor: "orange" }}>
-            COLUMNA DE LA IZQUIERDA NARANJA
-            {/* Contenido de la columna de 4 */}{" "}
-          </Col>{" "}
+            <Card>
+              
+              <Card.Header>Resumen de Compra</Card.Header>
+              <ListGroup variant="flush">
+                
+                <ListGroup.Item className="d-flex justify-content-between align-items-center">
+                  
+                  Productos <span>$valor</span>
+                </ListGroup.Item>
+                <ListGroup.Item className="d-flex justify-content-between align-items-center">
+                  
+                  Envíos <span>$2.000</span>
+                </ListGroup.Item>
+                <ListGroup.Item className="d-flex justify-content-between align-items-center">
+                  
+                  Descuentos <span>$0</span>
+                </ListGroup.Item>
+                <ListGroup.Item className="d-flex justify-content-between align-items-center">
+                
+                  Total <span>$Total</span>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  
+                  <Button variant="primary" className="w-100">
+                    Pagar
+                  </Button>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </Col>
           FINAL DEL ROW
         </Row>
         FINAL DEL CONTAINER
